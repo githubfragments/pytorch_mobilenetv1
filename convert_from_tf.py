a35e9ee2e94e1e362ea99f62cf7a1ea278e92370
alex <xqfone070@gmail.com>
2021-03-30 16:04:08 +0800

    mod mobilenet_v1

---
+++ convert_from_tf.py
@@ -0,0 +1,3 @@
+
+# https://www.cnblogs.com/daremosiranaihana/p/12833493.html
+# https://www.jianshu.com/p/0a61caeb693b
--- mobilenet_v1.py
+++ mobilenet_v1.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from typing import List, Any
 
 
 def conv_bn(inp, oup, stride=1):
@@ -23,36 +24,52 @@ def conv_dw(inp, oup, stride):
 
 
 # https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet_v1.py
+# https://github.com/pytorch/vision/blob/master/torchvision/models/mobilenetv2.py
 class MobileNetV1(nn.Module):
     def __init__(self,
                  num_classes=1000,
                  depth_multiplier=1.0,
                  min_depth=8):
         super(MobileNetV1, self).__init__()
-        depth = lambda d: max(int(d * depth_multiplier), min_depth)
-        self.mobelnet = nn.Sequential(
-            conv_bn(3, depth(32), 2),
-            conv_dw(depth(32), depth(64), 1),
-            conv_dw(depth(64), depth(128), 2),
-            conv_dw(depth(128), depth(128), 1),
-            conv_dw(depth(128), depth(256), 2),
-            conv_dw(depth(256), depth(256), 1),
-            conv_dw(depth(256), depth(512), 2),
-            conv_dw(depth(512), depth(512), 1),
-            conv_dw(depth(512), depth(512), 1),
-            conv_dw(depth(512), depth(512), 1),
-            conv_dw(depth(512), depth(512), 1),
-            conv_dw(depth(512), depth(512), 1),
-            conv_dw(depth(512), depth(1024), 2),
-            conv_dw(depth(1024), depth(1024), 1),
-            nn.AvgPool2d(7)
-        )
+        def depth(d): return max(int(d * depth_multiplier), min_depth)
+        features: List[nn.Module] = [conv_bn(3, depth(32), 2)]
+
+        conv_dw_setting = [
+            [32, 64, 1],
+            [64, 128, 2],
+            [128, 128, 1],
+            [128, 256, 2],
+            [256, 256, 1],
+            [256, 512, 2],
+            [512, 512, 1],
+            [512, 512, 1],
+            [512, 512, 1],
+            [512, 512, 1],
+            [512, 512, 1],
+            [512, 1024, 2],
+            [1024, 1024, 1]
+        ]
+
+        for in_c, out_c, stride in conv_dw_setting:
+            features.append(conv_dw(depth(in_c), depth(out_c), stride))
+
+        features.append(nn.AvgPool2d(7))
+        # make it nn.Sequential
+        self.mobilenet = nn.Sequential(*features)
 
         self.classifier = nn.Linear(depth(1024), num_classes)
 
     def forward(self, x):
-        x = self.mobelnet(x)
-        x = x.view(-1, 1024)
+        x = self.mobilenet(x)
         x = torch.flatten(x, 1)
         x = self.classifier(x)
         return x
+
+
+def mobilenet_v1(pretrained_weights=None, **kwargs: Any):
+    model = MobileNetV1(**kwargs)
+    if pretrained_weights is not None:
+        state_dict = torch.load(pretrained_weights)
+        model.load_state_dict(state_dict)
+    return model
+
